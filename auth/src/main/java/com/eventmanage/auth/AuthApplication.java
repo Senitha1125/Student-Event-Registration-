97163c4df6529621d9ef20492f1ca6e50a7fde73
Senitha Velmurugan <senithavelmurugan1125@gmail.com>
2026-04-17 22:39:44 +0530

    debug mongo uri

--- auth/src/main/java/com/eventmanage/auth/AuthApplication.java
+++ auth/src/main/java/com/eventmanage/auth/AuthApplication.java
@@ -2,6 +2,9 @@ package com.eventmanage.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AuthApplication {
@@ -10,4 +13,14 @@ public class AuthApplication {
         SpringApplication.run(AuthApplication.class, args);
     }
 
+
+    @Value("${spring.data.mongodb.uri:NOT_FOUND}")
+    private String mongoUri;
+
+    @Bean
+    public CommandLineRunner testMongoUri() {
+        return args -> {
+            System.out.println("🔥 Mongo URI loaded: " + mongoUri);
+        };
+    }
 }
