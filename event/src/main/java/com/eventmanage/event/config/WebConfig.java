ab589021a5767cb10c7ba7032c9404f10491d4d0
Senitha Velmurugan <senithavelmurugan1125@gmail.com>
2026-04-17 18:51:32 +0530

    global cors fix

--- event/src/main/java/com/eventmanage/event/config/WebConfig.java
+++ event/src/main/java/com/eventmanage/event/config/WebConfig.java
@@ -1,18 +1,23 @@
-package com.eventmanage.event.config;
+package com.eventmanage.auth.config; // change package per service
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig implements WebMvcConfigurer {
+public class WebConfig {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173") // Your React Port
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true);
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("https://studenteventmgtdepl-qqjjq83ub-sthuthi2410236-9833s-projects.vercel.app")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*");
+            }
+        };
     }
 }
